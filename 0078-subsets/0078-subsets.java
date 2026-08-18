af2f96fa33d16959f17dfa094422cc17b8c315a3
Sujal Neve <sujal.neve07@gmail.com>
2026-08-18 11:59:55 +0530

    Time: 1 ms (86.46%), Space: 44.2 MB (57.91%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrack(nums,0,new ArrayList<>(),ans);
+        return ans;
+
+        
+    }
+
+    public void backtrack(int[] nums,int start,List<Integer> curr,List<List<Integer>> ans)
+    {
+        // Evevry current subset is a valid answer
+        ans.add(new ArrayList<>(curr));
+
+        for(int i=start; i<nums.length;i++)
+        {
+            // choose
+            curr.add(nums[i]);
+            // explore
+            backtrack(nums,i+1,curr,ans);
+            //backtrack
+            curr.remove(curr.size()-1);
+            
+        }
+
+    }
+}
