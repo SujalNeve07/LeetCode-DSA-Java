9a6544501ecb80b7af7a6a914126807927a5a6d2
Sujal Neve <sujal.neve07@gmail.com>
2026-09-06 13:04:45 +0530

    Time: 1 ms (100%), Space: 46.6 MB (49.25%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -14,48 +14,73 @@
  * }
  */
 class Solution {
-    ArrayList<Integer> list = new ArrayList<>();
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode prev = null;
+    // without lambda function
+    // ArrayList<Integer> list = new ArrayList<>();
     public void recoverTree(TreeNode root) {
-        inorder(root);
+    //     inorder(root);
 
-        ArrayList<Integer> sorted = new ArrayList<>(list);
-        Collections.sort(sorted);
+    //     ArrayList<Integer> sorted = new ArrayList<>(list);
+    //     Collections.sort(sorted);
 
        
-        int a = 0, b = 0;
+    //     int a = 0, b = 0;
 
-        for (int i = 0; i < list.size(); i++) {
-            if (!list.get(i).equals(sorted.get(i))) {
-                if (a == 0)
-                    a = list.get(i);
-                else {
-                    b = list.get(i);
-                    break;
-                }
-            }
-        }
-        swap(root, a, b);
+    //     for (int i = 0; i < list.size(); i++) {
+    //         if (!list.get(i).equals(sorted.get(i))) {
+    //             if (a == 0)
+    //                 a = list.get(i);
+    //             else {
+    //                 b = list.get(i);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     swap(root, a, b);
         
+    // }
+    // void inorder(TreeNode root) {
+    //     if (root == null)
+    //         return;
+
+    //     inorder(root.left);
+    //     list.add(root.val);
+    //     inorder(root.right);
+    // }
+
+    // void swap(TreeNode root, int a, int b) {
+    //     if (root == null)
+    //         return;
+
+    //     if (root.val == a)
+    //         root.val = b;
+    //     else if (root.val == b)
+    //         root.val = a;
+
+    //     swap(root.left, a, b);
+    //     swap(root.right, a, b);
+    // }
+
+
+        helper(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
     }
-    void inorder(TreeNode root) {
-        if (root == null)
-            return;
 
-        inorder(root.left);
-        list.add(root.val);
-        inorder(root.right);
-    }
+    public void helper(TreeNode root){
+        if(root == null) return;
+        helper(root.left);
 
-    void swap(TreeNode root, int a, int b) {
-        if (root == null)
-            return;
-
-        if (root.val == a)
-            root.val = b;
-        else if (root.val == b)
-            root.val = a;
-
-        swap(root.left, a, b);
-        swap(root.right, a, b);
+        if(prev != null && prev.val >= root.val){
+            if(first == null){
+                first = prev;
+            }
+            second = root;
+        }
+        prev = root;
+        helper(root.right);
     }
 }
