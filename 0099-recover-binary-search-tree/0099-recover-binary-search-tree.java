9d815feff53a72fedc3e86fee04af33436cc3f1a
Sujal Neve <sujal.neve07@gmail.com>
2026-09-06 12:34:37 +0530

    Time: 5 ms (2.05%), Space: 46.3 MB (86.44%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+
+        ArrayList<Integer> sorted = new ArrayList<>(list);
+        Collections.sort(sorted);
+
+       
+        int a = 0, b = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (!list.get(i).equals(sorted.get(i))) {
+                if (a == 0)
+                    a = list.get(i);
+                else {
+                    b = list.get(i);
+                    break;
+                }
+            }
+        }
+        swap(root, a, b);
+        
+    }
+    void inorder(TreeNode root) {
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+
+    void swap(TreeNode root, int a, int b) {
+        if (root == null)
+            return;
+
+        if (root.val == a)
+            root.val = b;
+        else if (root.val == b)
+            root.val = a;
+
+        swap(root.left, a, b);
+        swap(root.right, a, b);
+    }
+}
